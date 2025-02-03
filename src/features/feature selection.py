33be7da04fc5749086a9f3432afd00008014dcd8
charliegotcodes <kevin.charles0164@gmail.com>
2025-02-02 23:13:03 -0500

    Files Updated

---
+++ src/features/feature selection.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from matplotlib import rcParams 
+from matplotlib.cm import rainbow 
+import seaborn as sns 
+
+import os
+from pandas import DataFrame
+import numpy as np
+
+from pathlib import Path
+
+current_dir = Path(__file__).resolve().parent.parent.parent
+
+def load_processed_data(filepath):
+    print("Load the Data")
+
+def correlation_analysis(df):
+    print("Correlation analysis of the dataframes")
+
+# Apply Multiple Feature Selection Methods 
+# 1: Chi-Square Test 
+# 2: 
+# 3: 
+
+
+
+
+def main():
+    print("Holder")
+
+
+
+if __name__ == "__main__":
+    main()
